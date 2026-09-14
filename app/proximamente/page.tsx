1e511a6ec1f944e152ba2ca00e5a484ffdbb2d9e
franram2000 <91743312+franram2000@users.noreply.github.com>
2026-09-14 10:46:05 +0200

    fix: load coming soon page styles

--- app/proximamente/page.tsx
+++ app/proximamente/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import Link from "next/link";
-import "../error-pages.css";
+import "./proximamente.css";
 
 export const metadata: Metadata = {
   title: "Próximamente",
